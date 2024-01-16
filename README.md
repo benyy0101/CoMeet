192d0a34d3b9230e3808d72ca82405a7481d3ed4
zerotay <zerogun1000@gmail.com>
2024-01-16 13:07:27 +0900

    Feat: 깃 설정파일 추가

---
+++ README.md
@@ -0,0 +1,6 @@
+# Developer Setting Guide
+1. execute settings/settings.sh
+```
+sh settings/settings.sh
+```
+
--- settings/settings.sh
+++ settings/settings.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+
+echo "initiating default git strategies" 
 cp -f prepare-commit-msg ../.git/hooks
