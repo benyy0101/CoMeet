0ce607eea3d1465f4cd9af8572335d253238d9f0
damongsanga <mike0413lata@gmail.com>
2024-01-17 10:25:58 +0900

    Feat: 기본 세팅

---
+++ back/src/test/java/com/a506/comeet/ComeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.a506.comeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
