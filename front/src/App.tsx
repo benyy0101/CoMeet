927cadf59982ec6562689dec22133aabfc0d1ef4
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2024-01-18 10:11:16 +0900

    S10P12A506-41 Feat: 방 생성페이지

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import tw from "tailwind-styled-components";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NewRoom from "./pages/NewRoom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          
+          <Route path="/new-room" element={<NewRoom />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
