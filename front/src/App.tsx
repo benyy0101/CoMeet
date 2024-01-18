014368e046aa21c3d067a324ee9a310433e66216
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2024-01-18 16:48:01 +0900

    S10P12A506-84 Feat: 모달 토글 구현

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -2,8 +2,15 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import NewRoom from "./pages/NewRoom";
 import ConditionCheck from "./pages/ConditionCheck";
+import Modal from "./components/Modal";
 
 function App() {
+  const [isModal, setIsModal] = React.useState<boolean>(false);
+
+  const modalHandler = () => {
+    setIsModal(!isModal);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
@@ -12,6 +19,8 @@ function App() {
           <Route path="/before-entrance" element={<ConditionCheck />}></Route>
         </Routes>
       </BrowserRouter>
+      <button onClick={modalHandler}> dhkfkfkfkf</button>
+      {isModal && <Modal toggleModal={modalHandler}/>}
     </div>
   );
 }
