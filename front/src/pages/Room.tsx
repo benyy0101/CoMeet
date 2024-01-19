c58a97e1dd00c4fa14b91241502dac78e7726fa7
jhahe <jhahe@naver.com>
2024-01-19 16:54:07 +0900

    Feat: 방 레이아웃 고침 & 멤버보기/환경설정 아이콘 추가

--- front/src/pages/Room.tsx
+++ front/src/pages/Room.tsx
@@ -7,15 +7,20 @@ import { Routes, Route, useParams } from "react-router-dom";
 import tw from "tailwind-styled-components";
 import { TailwindComponent } from "tailwind-styled-components/dist/tailwind";
 
+import Members from "../assets/img/members.svg";
+import Settings from "../assets/img/settings.svg";
+
 type styledType = TailwindComponent<React.HTMLAttributes<HTMLDivElement>, {}>;
 
 //css 스타일 컴포넌트 - 높이, 넓이 나중에 다시 다 고쳐야할 듯
 //StyleRoomOutline: 룸의 가장 바깥라인 - 높이 다시 고쳐야함!!
+//일단 텍스트 다 하얀색으로...
 const StyleRoomOutline: styledType = tw.div`
     flex
     justify-center
     h-[95vh]
-    pt-12
+    mt-2
+    text-white
 `;
 
 //StyleRoomLayout: 룸의 레이아웃
@@ -23,11 +28,14 @@ const StyleRoomLayout: styledType = tw.div`
     w-[98%]
     border-black-200
     border
+    bg-[#3B3B3B]
     rounded-xl`;
 
 //StyleRoomHead: 룸의 헤더 (방 이름, 공지버튼, 멤버보기버튼, 환경설정버튼)
 const StyleRoomHead: styledType = tw.div`
     flex justify-between
+    mt-3
+    mx-5
     `;
 
 //StyleRoomBoth: 네비바 & RTC 부분의 레이아웃
@@ -48,6 +56,7 @@ const StyleRoomRTCLayout: styledType = tw.div`
     flex
     items-center
     justify-center
+    
 
 `;
 
@@ -59,6 +68,8 @@ const StyleRoomRTC: styledType = tw.div`
     flex
     items-center
     justify-center
+    bg-[#282828]
+    rounded-md
     `;
 
 //Room 컴포넌트
@@ -93,7 +104,12 @@ export const Room = () => {
               SSAFY 10기 (방 이름) <button>!공지 버튼!</button>
             </div>
             <div>
-              <button>멤버보기</button> | <button>환경설정</button>
+              <button>
+                <img src={Members} width={25} alt="members" />
+              </button>
+              <button>
+                <img src={Settings} width={25} alt="settings" />
+              </button>
             </div>
           </StyleRoomHead>
           <StyleRoomBoth>
