09e331983e062e8dc045fa5d30d3892a0d7c8ea6
jhahe <jhahe@naver.com>
2024-01-19 15:45:53 +0900

    Feat: 메인페이지 간단한 예시 추가

--- front/src/pages/Mainpage.tsx
+++ front/src/pages/Mainpage.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import MainpageEx from "../assets/img/mainpage-ex.svg";
 
 export const Mainpage = () => {
-  return <div>Mainpage</div>;
+  return (
+    <div>
+      <img src={MainpageEx} alt="" />
+    </div>
+  );
 };
