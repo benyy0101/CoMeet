9d0f2b8ffc21382c6db3ba37b7f74ea8678f8970
damongsanga <mike0413lata@gmail.com>
2024-01-19 16:55:30 +0900

    S10P12A506-92 Test: Member 기본 CUD 구현

--- back/src/main/java/com/a506/comeet/member/entity/Member.java
+++ back/src/main/java/com/a506/comeet/member/entity/Member.java
@@ -1,9 +1,10 @@
 package com.a506.comeet.member.entity;
 
+import com.a506.comeet.common.enums.MemberAuthority;
 import com.a506.comeet.common.enums.MemberFeature;
-import com.a506.comeet.member.controller.MemberUpdateRequestDto;
+import com.a506.comeet.member.controller.dto.MemberSigninRequestDto;
+import com.a506.comeet.member.controller.dto.MemberUpdateRequestDto;
 import com.a506.comeet.common.BaseEntityWithSoftDelete;
-import com.a506.comeet.room.entity.Room;
 import com.a506.comeet.room.entity.RoomMember;
 import jakarta.persistence.*;
 import lombok.Builder;
@@ -30,14 +31,16 @@ public class Member extends BaseEntityWithSoftDelete {
     private String name;
     private String password;
     private String nickname;
-    private String link;
+    private String link = "default_link_letsgo";
     @Column(name = "profile_image")
-    private String profileImage;
+    private String profileImage = "default_profile_image_letsgo";
     private String email;
-    private String description;
-    private String authority;
+    private String description = "default_description_letsgo";
+
     @Enumerated(EnumType.STRING)
-    private MemberFeature feature;
+    private MemberAuthority authority = MemberAuthority.USER;
+    @Enumerated(EnumType.STRING)
+    private MemberFeature feature = MemberFeature.EARTH;
 
     @OneToMany(mappedBy = "member", fetch = FetchType.LAZY)
     private List<RoomMember> roomMembers = new ArrayList<>();
@@ -51,13 +54,13 @@ public class Member extends BaseEntityWithSoftDelete {
         this.email = email;
     }
 
-    public void updateUser(MemberUpdateRequestDto dto){
+    public void updateMember(MemberUpdateRequestDto dto){
         this.name = dto.getName();
         this.password = dto.getPassword();
         this.nickname = dto.getNickname();
         this.link = dto.getLink();
         this.profileImage = dto.getProfileImage();
-        this.email = dto.getProfileImage();
+        this.email = dto.getEmail();
         this.description = dto.getDescription();
         this.feature = dto.getFeature();
     }
@@ -69,6 +72,8 @@ public class Member extends BaseEntityWithSoftDelete {
     public void removeRoomMember(RoomMember roomMember){
         roomMembers.removeIf(rm -> rm.getMember().getMemberId().equals(roomMember.getMember().memberId) && rm.getRoom().getId().equals(roomMember.getRoom().getId()));
     }
+
+
 
 
 }
--- back/src/main/java/com/a506/comeet/member/repository/MemberRepository.java
+++ back/src/main/java/com/a506/comeet/member/repository/MemberRepository.java
@@ -5,7 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface MemberRepository extends JpaRepository<Member, String> {
+public interface MemberRepository extends JpaRepository<Member, String>, MemberCustomRepository{
 
     Optional<Member> findByMemberIdAndIsDeletedFalse(String memberId);
+
 }
