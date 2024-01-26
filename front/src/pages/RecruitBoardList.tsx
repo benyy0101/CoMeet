6dc6d14ec550cc77365f8c49d8f8bbba78ec2f68
jhahe <jhahe@naver.com>
2024-01-26 12:29:30 +0900

    Feat: 모집게시판, 자유게시판 링크 연결

--- front/src/pages/RecruitBoardList.tsx
+++ front/src/pages/RecruitBoardList.tsx
@@ -1,5 +1,18 @@
 import React from "react";
 
+import tw from "tailwind-styled-components";
+
 export const RecruitBoardList = () => {
-  return <div>RecruitBoardList</div>;
+  return (
+    <TotalContainer>
+      모집게시판
+      <button>글 등록</button>
+    </TotalContainer>
+  );
 };
+
+const TotalContainer = tw.div`
+flex
+w-full
+h-full
+`;
