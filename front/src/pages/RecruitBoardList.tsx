d3433fb150fcd7170e7219c0e5cf9f926042c348
jhahe <jhahe@naver.com>
2024-01-29 17:59:43 +0900

    Feat: 모집게시판 레이아웃 다시 구성 중 ing 집 가기 전 커밋

--- front/src/pages/RecruitBoardList.tsx
+++ front/src/pages/RecruitBoardList.tsx
@@ -15,15 +15,22 @@ export const RecruitBoardList = () => {
       <CenterTotalContainer>
         <CoreTotalContainer>
           <div>모집게시판 타이틀</div>
-          <div className="flex">
+          <BoardListHeader className="flex">
             여기 헤더임
             <div>키워드 검색</div>
             <div>검색바</div>
-          </div>
+          </BoardListHeader>
           <div className="flex flex-col">
             여기 리스트임
-            <RecruitBoardListLink />
+            {/* ReadButton은 임시! */}
+            <ReadButton>
+              <RecruitBoardListLink />
+            </ReadButton>
+            <ReadButton>
+              <RecruitBoardListLink />
+            </ReadButton>
           </div>
+          <div className="flex justify-center">페이지네이션</div>
         </CoreTotalContainer>
       </CenterTotalContainer>
       {/* 아래는 그냥 여백 때문에 */}
@@ -59,7 +66,7 @@ justify-center
 border
 `;
 
-//
+//진짜 모집게시판 리스트
 const CoreTotalContainer = tw.div`
 flex-grow
 max-w-[800px]
@@ -67,8 +74,11 @@ mb-5
 border
 `;
 
+//모집게시판 헤더 - 검색바, 키워드
+const BoardListHeader = tw.div``;
+
 // 임시
 // 글 보기 버튼
-const WriteButton = tw.button`
+const ReadButton = tw.button`
 hover:bg-gray-200
 `;
