d707878ff980359867f3633f94494b87177bc784
jhahe <jhahe@naver.com>
2024-01-29 13:51:03 +0900

    Feat: 방 정보 링크 이미지 넣고 css 꾸밈

--- front/src/components/BoardDetailRoomInfo.tsx
+++ front/src/components/BoardDetailRoomInfo.tsx
@@ -3,13 +3,15 @@ import React from "react";
 import tw from "tailwind-styled-components";
 
 import PoepleNumImg from "../assets/img/people-num.svg";
+import RoomImg from "../assets/img/room-default.png";
 
 export const BoardDetailRoomInfo: React.FC<{ roomId: string }> = (props) => {
   //방 ID로 방 조회해서 가져올 것들
   //방 제목
   const roomTitle = "서울 5반 알고리즘";
   //방 설명
-  const roomDescription = "B형을 위한 알고리즘 스터디방입니다";
+  const roomDescription =
+    "B형을 위한 알고리즘 스터디방입니다 .이것 저것 필수 저것도 필수 이것도 필수 이것도 필수 저건 선택입니다";
 
   const roomPeopleNum = "30 / 45";
 
@@ -21,41 +23,65 @@ export const BoardDetailRoomInfo: React.FC<{ roomId: string }> = (props) => {
   return (
     <RoomHyper href={roomLink} target="_blank" rel="noopener noreferrer">
       <TotalContainer>
-        <TitleAndNumContainer>
-          <RoomTitle>{roomTitle}</RoomTitle>
-          <RoomPeople>
-            <PeopleImg src={PoepleNumImg} alt="" />
-            <RoomPeopleNum>{roomPeopleNum}</RoomPeopleNum>
-          </RoomPeople>
-        </TitleAndNumContainer>
-        <RoomEx>{roomDescription}</RoomEx>
+        <RoomImgContainer src={RoomImg} alt="" />
+        <RoomInfo>
+          <TitleAndNumContainer>
+            <RoomTitle>{roomTitle}</RoomTitle>
+            <RoomPeople>
+              <PeopleImg src={PoepleNumImg} alt="" />
+              <RoomPeopleNum>{roomPeopleNum}</RoomPeopleNum>
+            </RoomPeople>
+          </TitleAndNumContainer>
+          <RoomEx>{roomDescription}</RoomEx>
 
-        <RoomHyperLink>{roomLink}</RoomHyperLink>
+          <RoomHyperLink>{roomLink}</RoomHyperLink>
+        </RoomInfo>
       </TotalContainer>
     </RoomHyper>
   );
 };
 
+//전체를 링크로 구성
 const RoomHyper = tw.a`
 `;
 
+//전체 컨테이너
 const TotalContainer = tw.div`
 flex
-flex-col
 mx-10
 mt-10
-p-7
+rounded-lg
 break-words
 bg-[#1F1C29]
-
 `;
 
+//룸 이미지 컨테이너인데 h-70으로 설정해뒀는데
+//룸 설명이 길어져서 전체 컨테이너가 70보다 길어지면 밑에가 뜬다
+//몇 줄 이상은 말줄임표로 구성한뒤 h 조절 필요
+const RoomImgContainer = tw.img`
+rounded-l-lg
+w-40
+h-70
+object-cover
+`;
+
+//이미지 제외 모든 정보
+const RoomInfo = tw.div`
+flex
+flex-col
+w-full
+p-7
+`;
+
+//제목과 인원수 컨테이너
 const TitleAndNumContainer = tw.div`
 flex
 w-full
 mb-3
+
 `;
 
+//방 제목
 const RoomTitle = tw.div`
 flex
 flex-grow
@@ -76,15 +102,18 @@ h-6
 mr-1
 `;
 
+//인원수
 const RoomPeopleNum = tw.div`
 items-end
 `;
 
+//방 설명
 const RoomEx = tw.div`
 text-gray-400
 mb-3
 `;
 
+//진짜 링크 부분
 const RoomHyperLink = tw.div`
 transition-colors
 hover:text-blue-500
