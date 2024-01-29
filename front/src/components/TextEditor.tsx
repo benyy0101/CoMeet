bf6b534469c0fb3b16f672d811cd3741ba49a078
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2024-01-29 12:27:59 +0900

    S10P12A506-169 Feat: Quill 컴포넌트 기본 구조 완료

--- front/src/components/TextEditor.tsx
+++ front/src/components/TextEditor.tsx
@@ -9,22 +9,57 @@ type QuillEditorProps = {
   setHtmlContent: string;
 };
 
-const SelectOption = {};
+type SelectOption = {
+  key: number;
+  value: string;
+  label: string;
+};
+
+const formats = [
+  "header",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+  "link",
+  "image",
+  "align",
+  "color",
+  "background",
+];
 
 function TextEditor() {
+  const [selectOption, setSelectOption] = React.useState<SelectOption[]>([
+    { key: 1, value: "question", label: "질문하기" },
+    { key: 2, value: "recruit", label: "구인구직" },
+    { key: 3, value: "tips", label: "팁/정보" },
+  ]);
+  const [headerTitle, setHeaderTitle] = React.useState<string>("자유게시판");
+
   return (
     <Wrapper>
+      <Header>
+        <HeaderTitle> {headerTitle}</HeaderTitle>
+      </Header>
       <TitleWrapper>
         <SelectForm>
-          <option value="quesiton">질문하기</option>
-          <option value="recruit">구인구직</option>
-          <option value="tips">팁/정보</option>
+          {selectOption.map((option) => (
+            <option value={option.value} key={option.key}>
+              {option.label}
+            </option>
+          ))}
         </SelectForm>
         <TitleInput type="text" placeholder="제목을 입력해 주세요"></TitleInput>
       </TitleWrapper>
-      <QuillEditor modules={module} />
+      <QuillContainer>
+        <Quill modules={module} formats={formats} />
+      </QuillContainer>
       <ButtonWrapper>
-        <button>등록하기</button>
+        <SubmitButton>작성하기</SubmitButton>
       </ButtonWrapper>
     </Wrapper>
   );
@@ -42,25 +77,36 @@ const module = {
 };
 
 const Wrapper = tw.div`
-    w-full
-    h-screen
-    shadow-md
-    bg-gradient-to-b
-    from-[#050110]
-    from-10%
-    to-[#1E0329]
-    p-2
+  flex
+  flex-col
+  items-center
+  justify-start
+  w-full
+  bg-gradient-to-b
+  min-h-screen
+  from-[#050110]
+  from-10%
+  to-[#1E0329]
+  p-2
+  gap-2
+  text-white
+`;
+
+const Header = tw.div`
+    text-4xl
+    my-4
+    w-3/4
     flex
-    flex-col
-    gap-2
-    items-center
     justify-center
-    text-white
+    items-center
+`;
+const HeaderTitle = tw.div`
 `;
 
 const SelectForm = tw.select`
-    text-black
+    text-white
     rounded-md
+    bg-black
 `;
 
 const TitleWrapper = tw.div`
@@ -82,18 +128,34 @@ const TitleInput = tw.input`
     bg-transparent
     focus:outline-none
 `;
-
-const QuillEditor = tw(ReactQuill)`
-    rounded-md
-    text-white
+const QuillContainer = tw.div`
     w-3/4
-    h-[80%]
-    
+    min-h-[300px]  
+    bg-transparent
+    rounded-md
+    mb-4           
+`;
+const Quill = tw(ReactQuill)`
+w-full
+h-[500px]
+`;
+const ButtonWrapper = tw.div`
+  w-3/4
+  my-10
+  flex
+  justify-end
+  pr-1
 `;
 
-const ButtonWrapper = tw.div`
-p-2
-bg-white
-
+const SubmitButton = tw.button`
+    text-white
+    font-bold
+    bg-gradient-to-r
+    from-purple-500
+    to-pink-500
+    py-2
+    px-4
+    rounded
+    cursor-pointer
 `;
 export default TextEditor;
