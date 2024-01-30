d542dba24de17a852e21079b6575efdfcbc4940b
damongsanga <mike0413lata@gmail.com>
2024-01-30 17:47:56 +0900

    S10P12A506-180 Feat: 방입장, 퇴장 Redis 관리 & 메타데이터 생성

--- back/src/main/java/com/a506/comeet/config/WebConfig.java
+++ back/src/main/java/com/a506/comeet/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("i10a506.p.ssafy.io:3000")
+                .allowedOrigins("i10a506.p.ssafy.io:3000", "http://localhost:3000")
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowedMethods("GET", "POST", "DELETE", "PATCH")
                 .allowCredentials(true);
