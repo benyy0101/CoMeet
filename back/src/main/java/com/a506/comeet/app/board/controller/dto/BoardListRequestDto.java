a59baed6c4e8cbf4ca165152ea598623fd90c4b0
heeyeon <kho200a@naver.com>
2024-01-31 18:02:41 +0900

    S10P12A506-216 Feat: BoardListRequestDto 수정

--- back/src/main/java/com/a506/comeet/app/board/controller/dto/BoardListRequestDto.java
+++ back/src/main/java/com/a506/comeet/app/board/controller/dto/BoardListRequestDto.java
@@ -1,5 +1,7 @@
 package com.a506.comeet.app.board.controller.dto;
 
+import com.a506.comeet.common.enums.BoardSearchType;
+import com.a506.comeet.common.enums.BoardSortType;
 import com.a506.comeet.common.enums.FreeBoardCategory;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,6 +11,9 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class BoardListRequestDto {
-    private FreeBoardCategory category;
-    private String keyword;
+    private Integer capacity; //최대 인원(가입한 인원 / 최대 인원)
+    private FreeBoardCategory category; //CHAT, TIP, QUESTION, PROMOTION
+    private BoardSearchType searchType;    //CONTENT, WRITER
+    private BoardSortType sortType;  //RECENT, LIKES, RECRUIT
+    private String keyword; //검색 내용
 }
---
+++ back/src/main/java/com/a506/comeet/common/enums/BoardSortType.java
@@ -0,0 +1,5 @@
+package com.a506.comeet.common.enums;
+
+public enum BoardSortType {
+    RECENT, LIKES, RECRUIT
+}
