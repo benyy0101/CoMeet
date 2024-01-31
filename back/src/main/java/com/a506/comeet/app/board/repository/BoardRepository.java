c62da941fed469fba2226c3bc0168823ccb07ee1
heeyeon <kho200a@naver.com>
2024-01-31 10:58:07 +0900

    S10P12A506-147 Docs: querydsl 주석

--- back/src/main/java/com/a506/comeet/app/board/repository/BoardRepository.java
+++ back/src/main/java/com/a506/comeet/app/board/repository/BoardRepository.java
@@ -1,7 +1,6 @@
 package com.a506.comeet.app.board.repository;
 
 import com.a506.comeet.app.board.entity.Board;
-import com.a506.comeet.app.room.repository.RoomRepositoryCustom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- back/src/main/java/com/a506/comeet/app/board/repository/BoardRepositoryCustomImpl.java
+++ back/src/main/java/com/a506/comeet/app/board/repository/BoardRepositoryCustomImpl.java
@@ -1,6 +1,5 @@
 package com.a506.comeet.app.board.repository;
 
-import com.a506.comeet.app.board.controller.dto.BoardDetailResponseDto;
 import com.a506.comeet.app.board.controller.dto.BoardListRequestDto;
 import com.a506.comeet.app.board.controller.dto.BoardListResponseDto;
 import com.a506.comeet.app.board.entity.Board;
@@ -8,7 +7,6 @@ import com.a506.comeet.app.keyword.entity.RoomKeyword;
 import com.a506.comeet.app.room.entity.Room;
 import com.a506.comeet.common.enums.FreeBoardCategory;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
@@ -28,27 +26,31 @@ import static com.a506.comeet.app.board.entity.QBoard.*;
 @Repository
 public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
 
-    private final JPAQueryFactory jpaQueryFactory;
+    private final JPAQueryFactory jpaQueryFactory; // JPA 쿼리를 생성하고 실행하는데 사용
 
     @Override
     public Page<BoardListResponseDto> searchBoardCustom(BoardListRequestDto req, Pageable pageable) {
+		// 쿼리 설정
         JPAQuery<Board> query = jpaQueryFactory
-            .selectFrom(board)
+            .selectFrom(board) // board와 writer 조인
             .leftJoin(board.writer).fetchJoin()
-            .where(eqCategory(req.getCategory()), eqKeyword(req.getKeyword()));
+            .where(eqCategory(req.getCategory()), eqKeyword(req.getKeyword())); // 조건
 
-        long total = query.fetchCount(); // 전체 항목 수 계산
+        long total = query.fetchCount(); // 전체 게시물 수
 
+		// 페이징된 게시물 조회
 		List<Board> boards = query
-			.offset(pageable.getOffset())
-			.limit(pageable.getPageSize())
+			.offset(pageable.getOffset()) //반환되는 행의 시작점
+			.limit(pageable.getPageSize())	//반환되는 행의 수
 			.orderBy(board.createdAt.desc())
 			.fetch();
 
+		//Dto 변환
 		List<BoardListResponseDto> res = boards.stream().map(board ->
 			BoardListResponseDto.toBoardListResponseDto(board, board.getRoom(), board.getWriter(), getKeywordsString(board.getRoom()))
 		).collect(Collectors.toList());
 
+		//페이지 객체 반환
         return new PageImpl<>(res, pageable, total);
     }
 
@@ -56,14 +58,13 @@ public class BoardRepositoryCustomImpl implements BoardRepositoryCustom {
 		if (category == null) {
 			return null;
 		}
-
 		return board.category.eq(category);
 	}
 
 	private BooleanExpression eqKeyword(String keyword){
 		if(keyword == null || keyword.isEmpty() || keyword.isBlank())
 			return null;
-		return board.title.containsIgnoreCase(keyword);
+		return board.title.containsIgnoreCase(keyword); //대소문자 구분하지 않는 부분 문자열 검색
 	}
 
 	private String getKeywordsString(Room room){
