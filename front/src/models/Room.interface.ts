649ec1c8803340c2a5215b19bcd94532968bcd04
zerotay <zerogun1000@gmail.com>
2024-02-02 18:00:42 +0900

    S10P12A506-256 Feat: 방 변경 거의 완성

--- front/src/models/Room.interface.ts
+++ front/src/models/Room.interface.ts
@@ -1,20 +1,23 @@
 import { ROOM_CONSTRAINTS, ROOM_TYPE } from "models/Enums.type";
 
 export interface CreateRoomParams {
-  title: string;
-  description: string;
-  capacity: number;
-  constraints: ROOM_CONSTRAINTS;
-  type: ROOM_TYPE;
+  title: string; //필수
+  description: string; //선택
+  capacity: number; // 1~20
+  constraints: ROOM_CONSTRAINTS; //필수
+  type: ROOM_TYPE; //필수
 }
 
 export interface ModifyRoomParams {
-  roomId: number;
-  title: string;
-  description: string;
-  capacity: number;
-  notice: string;
-  password: string;
-  constraints: ROOM_CONSTRAINTS;
-  keywordIds: number[];
+  roomId: number; //필수
+  managerId?: string | undefined; //매니저 변경 시에만 삽입
+  title: string; //필수
+  description: string; //선택
+  roomImage: string; //필수
+  capacity: number; // 1~20
+  notice: string; //1000자 제한
+  isLocked: Boolean; //필수
+  password: string; //isLocked가 True일 때 4~8
+  constraints: ROOM_CONSTRAINTS; //필수
+  keywordIds: number[]; //선택
 }
--- front/src/utils/test/Room/ModifyRoom.ts
+++ front/src/utils/test/Room/ModifyRoom.ts
@@ -10,24 +10,33 @@ export function func(lst: any): any {
 }
 
 const data1: ModifyRoomParams = {
-  roomId: 10,
+  roomId: 4,
+  managerId: "movinggun",
   title: "테스트 수정",
-  description: "방에 대한 설명234",
+  description: "",
+  roomImage: "ts",
   capacity: 13,
   notice: "공지데스",
+  isLocked: true,
   password: "asdf1234",
   constraints: "VIDEOON",
   keywordIds: [2, 3, 4],
 };
+
+//무빙건으로 테스트 시 이건 FAIL이 떠야 정상
 const data2: ModifyRoomParams = {
   roomId: 1,
   title: "김수정",
   description: "설명 수정",
+  roomImage: "ts",
   capacity: 15,
   notice: "공지",
+  isLocked: false,
   password: "",
   constraints: "VIDEOON",
   keywordIds: [2, 3, 4],
 };
+
+console.log("asdf", data2);
 
 export let datas: Array<any> = [data1, data2];
