96b98b90158c604438ab70bb7aa5386edf1ce75d
Zerotay <67823010+Zerotay@users.noreply.github.com>
2024-02-04 13:28:42 +0900

    Feat: 라운지 CUD api 연결

--- front/src/models/Lounge.interface.ts
+++ front/src/models/Lounge.interface.ts
@@ -1,5 +1,20 @@
-interface example {
-  ex1: string;
-  ex2: number;
+export interface CreateLoungeParams {
+  roomId: number; //필수
+  name: string; //필수
 }
-export {};
+
+export interface CreateLoungeResponse {
+  loungeId: number; //필수
+}
+export interface ModifyLoungeParams {
+  loungeId: number; //필수
+  name: string; //필수
+}
+
+export interface ModifyLoungeResponse {}
+
+export interface DeleteLoungeParams {
+  loungeId: number; //필수
+}
+
+export interface DeleteLoungeResponse {}
