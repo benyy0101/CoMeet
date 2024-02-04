5ecb11fbfa562c283118a3bbfef0a80dcde1440b
Zerotay <67823010+Zerotay@users.noreply.github.com>
2024-02-04 11:31:31 +0900

    Feat: 방 입장,퇴장 api 연결

--- front/src/api/Room.ts
+++ front/src/api/Room.ts
@@ -1,6 +1,8 @@
 import {
   CreateRoomParams,
   DeleteRoomParams,
+  EnterRoomParams,
+  LeaveRoomParams,
   ModifyRoomParams,
   PermitJoinRoomParams,
   SearchRoomParams,
@@ -12,7 +14,7 @@ import { makeQuerystring } from "utils/ApiUtil";
 export const createRoom = async (params: CreateRoomParams) => {
   const url = `room`;
   const response = await localAxios.post(url, params);
-  return response.data; // The token
+  return response.data;
 };
 
 export const modifyRoom = async (params: ModifyRoomParams) => {
@@ -21,13 +23,13 @@ export const modifyRoom = async (params: ModifyRoomParams) => {
   //roomId가 같이 바디에 들어가도 문제가 안 생긴다
   const response = await localAxios.patch(url, params);
 
-  return response.data; // The token
+  return response.data;
 };
 
 export const searchRoom = async (params: SearchRoomParams) => {
   const url = `room${makeQuerystring(params)}`;
   const response = await localAxios.get(url);
-  return response.data; // The token
+  return response.data;
 };
 
 export const deleteRoom = async (params: DeleteRoomParams) => {
@@ -35,7 +37,7 @@ export const deleteRoom = async (params: DeleteRoomParams) => {
   const url = `room/${roomId}`;
   const response = await localAxios.delete(url);
 
-  return response.data; // The token
+  return response.data;
 };
 
 export const permitJoinRoom = async (params: PermitJoinRoomParams) => {
@@ -44,7 +46,7 @@ export const permitJoinRoom = async (params: PermitJoinRoomParams) => {
   const body = { memberId };
   const response = await localAxios.post(url, body);
 
-  return response.data; // The token
+  return response.data;
 };
 
 export const withdrawRoom = async (params: WithdrawRoomParams) => {
@@ -52,5 +54,21 @@ export const withdrawRoom = async (params: WithdrawRoomParams) => {
   const url = `room/join/${roomId}`;
   const response = await localAxios.delete(url);
 
-  return response.data; // The token
+  return response.data;
+};
+
+export const enterRoom = async (params: EnterRoomParams) => {
+  const { roomId } = params;
+  const url = `room/${roomId}/enter`;
+  const response = await localAxios.post(url, params);
+
+  return response.data;
+};
+
+export const leaveRoom = async (params: LeaveRoomParams) => {
+  const { roomId } = params;
+  const url = `room/${roomId}/enter`;
+  //이거 잘되는지 확인 필요
+  const response = await localAxios.delete(url, { data: params });
+  return response.data;
 };
--- front/src/models/Room.interface.ts
+++ front/src/models/Room.interface.ts
@@ -21,7 +21,16 @@ export interface ModifyRoomParams {
   constraints: ROOM_CONSTRAINTS; //필수
   keywordIds?: number[]; //선택
 }
-
+/**
+ * searchKeyword?: string; //선택
+ * managerNickname?: string; //선택
+ * isLocked?: Boolean; //선택
+ * keywordIds?: number[]; //선택
+ * constraints?: ROOM_CONSTRAINTS; //선택
+ * sortBy?: ROOM_SORTBY; //선택
+ * page?: number; //선택 맞나..? 인덱스 0부터 시작
+ * size?: number; //선택
+ */
 export interface SearchRoomParams {
   searchKeyword?: string; //선택
   managerNickname?: string; //선택
@@ -44,4 +53,17 @@ export interface PermitJoinRoomParams {
 
 export interface WithdrawRoomParams {
   roomId: number; //필수
+}
+/**
+ * roomId: number; //필수
+ * password: string | null; //필수. 없으면 null이라도 넣어야함
+ */
+export interface EnterRoomParams {
+  roomId: number; //필수
+  password: string | null; //필수. 없으면 null이라도 넣어야함
+}
+
+export interface LeaveRoomParams {
+  roomId: number; //필수
+  keywords?: string; //선택. 키워드명을 공백으로 구분한 문자열로
 }
