3c3a87a3ff942880f113ad485e5f16cf49499806
zerotay <zerogun1000@gmail.com>
2024-02-05 17:25:23 +0900

    S10P12A506-209 Feat: 게시판 관련 이넘 타입 추가

--- front/src/models/Enums.type.ts
+++ front/src/models/Enums.type.ts
@@ -1,6 +1,27 @@
-// 방을 만들 때의 제약조건
+/**
+ * 방을 만들 때의 제약조건
+ */
 export type ROOM_CONSTRAINTS = "VIDEOONMICOFF" | "VIDEOON" | "MICOFF" | "FREE";
-// 방의 타입. 일회용, 영구용
+/**
+ * 방의 타입. 일회용, 영구용
+ */
 export type ROOM_TYPE = "DISPOSABLE" | "PERMANENT";
-// 방 정렬 방식. CURRENT_PEOPLE은 아직 구현 안 됨
+/**
+ * 방 정렬 방식. CURRENT_PEOPLE은 아직 구현 안 됨
+ */
 export type ROOM_SORTBY = "LATEST" | "OLDEST" | "CURRENT_PEOPLE";
+
+/**
+ * 게시판 기본 타입. 자유글, 모집글
+ */
+export type BOARD_TYPE = "FREE" | "RECRUIT";
+
+/**
+ * 자유게시판 카테고리. 잡담, 팁, 질문, 홍보, 인기
+ */
+export type BOARD_CATEGORY =
+  | "CHAT"
+  | "TIP"
+  | "QUESTION"
+  | "PROMOTION"
+  | "POPULAR";
