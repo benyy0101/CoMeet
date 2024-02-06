0b88e84aae069f48156a6a7a724423953c8f3b85
이유로 <eurohand@naver.com>
2024-02-06 10:43:41 +0900

    S10P12A506-270 Feat: 채널 입장 중 라운지 입장 구현

--- front/src/components/Room/LoungeButton.tsx
+++ front/src/components/Room/LoungeButton.tsx
@@ -1,44 +1,38 @@
 import { ChatBubbleOvalLeftEllipsisIcon } from "@heroicons/react/24/solid";
 import { ILounge } from "models/Lounge.interface";
-import { useState } from "react";
 import tw from "tailwind-styled-components";
 
 interface IProps {
   lounge: ILounge;
+  active: boolean;
   disabled: boolean;
   moveLounge: (lounge: ILounge) => void;
 }
 
-export default function LoungeButton({ lounge, disabled, moveLounge }: IProps) {
-  const [isIn, setIsIn] = useState<boolean>(false);
-  const isInHandler = () => {
-    setIsIn(!isIn);
-  };
+export default function LoungeButton({ lounge, active, disabled, moveLounge }: IProps) {
   return (
     <LoungeButtonContainer>
       <IconButton
+        $active={active}
         disabled={disabled}
         onClick={() => {
           moveLounge(lounge);
         }}
-        onMouseEnter={isInHandler}
-        onMouseLeave={isInHandler}
       >
         <ChatBubbleOvalLeftEllipsisIcon className="text-white w-8 h-8" />
-        {isIn ? <LoungeButtonTitle>{lounge.name}</LoungeButtonTitle> : null}
+        <LoungeButtonTitle>{lounge.name}</LoungeButtonTitle>
       </IconButton>
     </LoungeButtonContainer>
   );
 }
 
-const IconButton = tw.button`
+const IconButton = tw.button<{ $active: boolean }>`
 w-10
 h-10
 flex
 justify-center
 items-center
-bg-slate-800
-disabled:bg-slate-500
+${(p) => (p.$active ? "bg-purple-600" : "bg-slate-800")}
 rounded-full
 text-3xl
 cursor-pointer
@@ -51,6 +45,7 @@ const LoungeButtonContainer = tw.div`
 flex
 flex-col
 items-center
+group
 `;
 
 const LoungeButtonTitle = tw.h1`
@@ -66,4 +61,6 @@ py-2
 rounded-md
 border
 border-[#d9e5db]
+hidden
+group-hover:block
 `;
