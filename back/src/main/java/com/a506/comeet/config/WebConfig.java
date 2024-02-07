fac9a7e8977b5a358bf7790f221bca7cf2a2ce5a
damongsanga <mike0413lata@gmail.com>
2024-02-07 16:59:38 +0900

    S10P12A506-322 Feat: Referrer 검증 로직 개발 테스트를 위해 주석 처리. 배포시 원복 필요

--- back/src/main/java/com/a506/comeet/config/WebConfig.java
+++ back/src/main/java/com/a506/comeet/config/WebConfig.java
@@ -1,8 +1,10 @@
 package com.a506.comeet.config;
 
-import com.a506.comeet.auth.ReferrerCheckInterceptor;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.*;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
@@ -15,9 +17,9 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
 //    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new ReferrerCheckInterceptor());
-    }
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(new ReferrerCheckInterceptor());
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
