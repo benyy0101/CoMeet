e4e76ba47d6826bb13b45ec45bffd16c9ab7c0c8
jhahe <jhahe@naver.com>
2024-02-08 14:16:21 +0900

    Feat: 캘린더 라이브러리 & styled-component 설치

--- front/src/components/Mypage/MyTILCalendar.tsx
+++ front/src/components/Mypage/MyTILCalendar.tsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 import tw from "tailwind-styled-components";
 
+type ValuePiece = Date | null;
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
 export const MyTILCalendar = () => {
-  return <TotalContainer>MyTILCalendar - 캘린더 API</TotalContainer>;
+  const [today, setToday] = useState<Value>(new Date());
+
+  return (
+    <TotalContainer>
+      <div>
+        <StyledCalendar locale="en" onChange={setToday} value={today} />
+      </div>
+    </TotalContainer>
+  );
 };
 
 const TotalContainer = tw.div`
 text-white
 w-full
 h-full
+`;
+
+const StyledCalendar = tw(Calendar)`
+text-black
+
 `;
