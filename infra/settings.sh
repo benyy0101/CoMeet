2a0a74fc22c71c3e497e6d9143c3c9f5bb499ef9
Zerotay <67823010+Zerotay@users.noreply.github.com>
2024-02-12 21:02:03 +0900

    S10P12A506-321 Feat: 인프라 세팅 파일 어디서든 잘 되도록 변경 & 대소문자 파일 이름 구분하도록 변경

--- infra/settings.sh
+++ infra/settings.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 
-echo "initiating default git strategies" 
-cp -f ./infra/prepare-commit-msg ./.git/hooks
-if [ $? -ne 0 ]; then
-    echo "에러가 발생했습니다! 깃 저장소의 최상단 디렉에서 명령을 실행해주세요!
-    문제가 해결되지 않으면 인프라 담당자에게 문의 바랍니다!
-    "
+GIT_ROOT_DIRECTORY=$(git rev-parse --show-toplevel)
+echo ${GIT_ROOT_DIRECTORY}
+
+echo "step 1. Configurating git hooks" 
+cp -f ${GIT_ROOT_DIRECTORY}/infra/prepare-commit-msg ${GIT_ROOT_DIRECTORY}/.git/hooks
+if [ $? -ne 0 ]; 
+then
+    echo "Error occured. Contact to infra engineer in your team."
+    exit 1
+else
+    echo "Git hook config done."
+fi
+
+echo "step 2. Identifying diff between bigcase and smallcase" 
+git config core.ignorecase false
+if [ $? -ne 0 ]; 
+then
+    echo "Error occured. Contact to infra engineer in your team."
+    exit 1
+else
+    echo "Git ignorecase config done."
 fi
