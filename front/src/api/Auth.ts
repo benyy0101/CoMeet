c60b057bd643cd70908453ea9808f865fe02005f
benyy0101 <65917479+benyy0101@users.noreply.github.com>
2024-02-13 13:00:03 +0900

    S10P12A506-337 Feat: 깃허브 로그인 버튼 추가

--- front/src/api/Auth.ts
+++ front/src/api/Auth.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import { localAxios } from "./http-commons";
 import { LoginResponse } from "models/Login.interface";
 
@@ -15,4 +16,10 @@ export const handleLogin = async (
 export const handleLogout = async () => {
   const response = await localAxios.post("/auth/logout");
   return response;
+};
+export const githubLogin = async (): Promise<LoginResponse> => {
+  const response = await axios.get(
+    "https://github.com/login/oauth/authorize?client_id=ee190e90e2c248f7e25d&scope=user:email"
+  );
+  return response.data;
 };
