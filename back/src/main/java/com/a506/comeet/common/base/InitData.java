53059b8ab6df3efaa9ae8663df61d91276496dbe
zerotay <zerogun1000@gmail.com>
2024-02-14 18:21:30 +0900

    Feat: adding init data

--- back/src/main/java/com/a506/comeet/common/base/InitData.java
+++ back/src/main/java/com/a506/comeet/common/base/InitData.java
@@ -75,6 +75,7 @@ public class InitData {
 
         createMember("damongsanga", "덕주", "QWER1234!", "damongsanga@naver.com", "다몽상가");
         createMember("movingGun", "동건", "QWER1234!", "movingGun@naver.com", "무빙건");
+        createMember("donggun1000", "김동건", "QWER1234!", "donggun1000@naver.com", "제로타이");
         createMember("hyeeyon", "희연", "QWER1234!", "hyeeyon@naver.com", "희여니");
         createMember("yuroyuro", "유로", "QWER1234!", "yuroyuro@naver.com", "너는무료나는유로");
         createMember("benny", "태수", "QWER1234!", "benny@naver.com", "커피마니아");
@@ -92,6 +93,7 @@ public class InitData {
         // 일회용방 생성
         createRoom("damongsanga", "자바 기초 스터디", "자바 프로그래밍을 처음 배우는 사람들을 위한 스터디방입니다.", 5, RoomConstraints.VIDEOONMICOFF, RoomType.DISPOSABLE, List.of(1L));
         createRoom("movingGun", "Node.js 개발자 모임", "Node.js 개발 논의를 위한 전용 공간입니다.", 6, RoomConstraints.VIDEOON, RoomType.DISPOSABLE, Arrays.asList(2L, 3L));
+        createRoom("donggun1000", "B형 준비하시는 분들 모이세요 4시간 ㄱㄱ" "후딱 달리죠.", 6, RoomConstraints.VIDEOON, RoomType.DISPOSABLE, Arrays.asList(2L, 3L));
         createRoom("hyeeyon", "리액트 인터랙티브 워크샵", "중급 학습자를 위한 인터랙티브 리액트 워크샵입니다.", 10, RoomConstraints.MICOFF, RoomType.DISPOSABLE, Arrays.asList(4L, 5L));
         createRoom("yuroyuro", "풀스택 개발 토론", "풀스택을 사용한 풀스택 개발에 대해 토론하는 장소입니다.", 9, RoomConstraints.FREE, RoomType.DISPOSABLE, Arrays.asList(1L, 2L, 3L));
         createRoom("benny", "파이썬 데이터 사이언스 학습", "파이썬을 사용한 데이터 사이언스 프로젝트에 대해 배우고 경험을 공유합니다.", 8, RoomConstraints.FREE, RoomType.DISPOSABLE, Arrays.asList(1L, 2L, 3L, 4L, 5L));
