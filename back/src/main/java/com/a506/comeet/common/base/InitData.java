e3774bf0a7971264c449b31a98085fa92082a825
damongsanga <mike0413lata@gmail.com>
2024-02-15 13:12:03 +0900

    Hotfix: initdata 수정

--- back/src/main/java/com/a506/comeet/common/base/InitData.java
+++ back/src/main/java/com/a506/comeet/common/base/InitData.java
@@ -74,7 +74,7 @@ public class InitData {
         TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 
         createMember("damongsanga", "덕주", "QWER1234!", "damongsanga@naver.com", "다몽상가");
-        createMember("movingGun", "동건", "QWER1234!", "movingGun@naver.com", "무빙건");
+        createMember("movinggun", "동건", "QWER1234!", "movinggun@naver.com", "무빙건");
         createMember("hyeeyon", "희연", "QWER1234!", "hyeeyon@naver.com", "희여니");
         createMember("yuroyuro", "유로", "QWER1234!", "yuroyuro@naver.com", "너는무료나는유로");
         createMember("benny", "태수", "QWER1234!", "benny@naver.com", "커피마니아");
@@ -110,7 +110,7 @@ public class InitData {
 
         // 일회용방 생성 (1~6)
         createRoom("damongsanga", "자바 기초 스터디", "자바 프로그래밍을 처음 배우는 사람들을 위한 스터디방입니다.", 5, RoomConstraints.VIDEOONMICOFF, RoomType.DISPOSABLE, List.of(1L));
-        createRoom("movingGun", "Node.js 개발자 모임", "Node.js 개발 논의를 위한 전용 공간입니다.", 6, RoomConstraints.VIDEOON, RoomType.DISPOSABLE, Arrays.asList(2L, 3L));
+        createRoom("movinggun", "Node.js 개발자 모임", "Node.js 개발 논의를 위한 전용 공간입니다.", 6, RoomConstraints.VIDEOON, RoomType.DISPOSABLE, Arrays.asList(2L, 3L));
         createRoom("hyeeyon", "리액트 인터랙티브 워크샵", "중급 학습자를 위한 인터랙티브 리액트 워크샵입니다.", 10, RoomConstraints.MICOFF, RoomType.DISPOSABLE, Arrays.asList(4L, 5L));
         createRoom("yuroyuro", "풀스택 개발 토론", "풀스택을 사용한 풀스택 개발에 대해 토론하는 장소입니다.", 9, RoomConstraints.FREE, RoomType.DISPOSABLE, Arrays.asList(1L, 2L, 3L));
         createRoom("benny", "파이썬 데이터 사이언스 학습", "파이썬을 사용한 데이터 사이언스 프로젝트에 대해 배우고 경험을 공유합니다.", 8, RoomConstraints.FREE, RoomType.DISPOSABLE, Arrays.asList(1L, 2L, 3L, 4L, 5L));
@@ -118,7 +118,7 @@ public class InitData {
 
         // 지속방 생성 (7~13)
         createRoom("damongsanga", "블록체인 기초", "블록체인 개념과 암호화 알고리즘에 대한 초보자 친화적 논의입니다.", 5, RoomConstraints.VIDEOONMICOFF, RoomType.PERMANENT, List.of(7L));
-        createRoom("movingGun", "자바스크립트 고급 기술 탐구", "자바스크립트 프로그래밍의 고급 개념을 탐구합니다.", 6, RoomConstraints.VIDEOON, RoomType.PERMANENT, Arrays.asList(2L, 3L, 5L));
+        createRoom("movinggun", "자바스크립트 고급 기술 탐구", "자바스크립트 프로그래밍의 고급 개념을 탐구합니다.", 6, RoomConstraints.VIDEOON, RoomType.PERMANENT, Arrays.asList(2L, 3L, 5L));
         createRoom("hyeeyon", "웹 개발 입문", "HTML, CSS, 자바스크립트를 이용한 웹 개발 초보자 가이드입니다.", 10, RoomConstraints.MICOFF, RoomType.PERMANENT, Arrays.asList(4L, 5L));
         createRoom("yuroyuro", "AWS 클라우드 컴퓨팅", "아마존 웹 서비스를 이용한 클라우드 컴퓨팅 서비스에 대한 토론입니다.", 9, RoomConstraints.FREE, RoomType.PERMANENT, Arrays.asList(1L, 2L, 3L));
         createRoom("benny", "사이버 보안 기초", "사이버 보안의 기초와 온라인 존재를 보호하는 방법을 배웁니다.", 8, RoomConstraints.FREE, RoomType.PERMANENT, Arrays.asList(1L, 2L, 3L, 4L, 5L));
@@ -133,7 +133,7 @@ public class InitData {
         createRoom("iamaltman", "LLM 논문 읽기", "매주 수요일, 1개 신규로 발행된 LLM 관련 논문을 읽고 내용을 공유합니다", 4, RoomConstraints.FREE, RoomType.PERMANENT, Arrays.asList(11L));
         createRoom("hyeonah", "프론트 스터디", "프레임워크에 한정되지 않은 프론트 스터디입니다", 10, RoomConstraints.MICOFF, RoomType.PERMANENT, Arrays.asList(3L, 5L, 13L));
         createRoom("iammusk", "남자라면 땀내나는 C를!", "진득하게 C 부터 다시 시작해보는 스터디입니다", 10, RoomConstraints.VIDEOON, RoomType.PERMANENT, Arrays.asList(12L));
-        createRoom("movingGun", "[데브옵스] 주말 인프라 스터디", "토요일 오전마다 운영되는 인프라 스터디입니다", 10, RoomConstraints.FREE, RoomType.PERMANENT, Arrays.asList(9L, 10L));
+        createRoom("movinggun", "[데브옵스] 주말 인프라 스터디", "토요일 오전마다 운영되는 인프라 스터디입니다", 10, RoomConstraints.FREE, RoomType.PERMANENT, Arrays.asList(9L, 10L));
 
         // 추가 일회용방 생성 (순서 바꾸면 안됨!!)
         createRoom("iammusk", "자바 스터디", "내 손을 java바~", 6, RoomConstraints.FREE, RoomType.DISPOSABLE, Arrays.asList(1L));
@@ -147,14 +147,14 @@ public class InitData {
 
         // 자유게시판
         createBoard("damongsanga", "자바 공부법", "자바 프로그래밍을 처음 배우는 사람들을 위한 스터디 방법입니다.",  BoardType.FREE, FreeBoardCategory.CHAT, null);
-        createBoard("movingGun", "개발자의 일상", "개발하면서 겪은 재미있는 일화를 공유해요. 모두의 이야기를 기다립니다.",  BoardType.FREE, FreeBoardCategory.CHAT, null);
+        createBoard("movinggun", "개발자의 일상", "개발하면서 겪은 재미있는 일화를 공유해요. 모두의 이야기를 기다립니다.",  BoardType.FREE, FreeBoardCategory.CHAT, null);
         createBoard("hyeeyon", "파이썬 데이터 분석 질문", "데이터 분석 중에 마주친 문제에 대해 질문합니다. 판다스 사용법이 궁금해요!",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
         createBoard("yuroyuro", "개발자를 위한 꿀팁!", "개발을 하면서 알게 된 유용한 팁들을 공유합니다. 시간 절약하는 팁 대환영!",  BoardType.FREE, FreeBoardCategory.TIP, null);
         createBoard("hyeonah", "내 프로젝트 홍보", "새로 시작한 프로젝트를 소개하고 팀원을 모집합니다. 관심 있으신 분들은 연락주세요!",  BoardType.FREE, FreeBoardCategory.PROMOTION, null);
 
         createBoard("damongsanga", "풀스택이라고 하셨잖아요!", "네. 풀스택 오버플로우요",  BoardType.FREE, FreeBoardCategory.CHAT, null);
         createBoard("hyeonah", "왜 개발자들은 다크모드를 좋아할까요?", "light 모드는 bug를 끌어들이니까요.. 살려주세요..",  BoardType.FREE, FreeBoardCategory.CHAT, null);
-        createBoard("movingGun", "오늘 운동 뭐하지", "어제 하체 맛있게 먹었으니 오늘은 등으로 간다",  BoardType.FREE, FreeBoardCategory.CHAT, null);
+        createBoard("movinggun", "오늘 운동 뭐하지", "어제 하체 맛있게 먹었으니 오늘은 등으로 간다",  BoardType.FREE, FreeBoardCategory.CHAT, null);
         createBoard("benny", "곧 커피 엑스포가 열린대요", "90% 얼리버드 개꿀. 같이 가실분 구합니다!",  BoardType.FREE, FreeBoardCategory.CHAT, null);
         createBoard("iamaltman", "반도체 수급이 부족해요", "다시 회사로 복귀도 했으니 800조 정도는 투자를 받아야겠어요~",  BoardType.FREE, FreeBoardCategory.CHAT, null);
         createBoard("hyeeyon", "솔직히 생각해도", "로고는 너무 잘 뽑은 것 같아요 그렇죠?",  BoardType.FREE, FreeBoardCategory.CHAT, null);
@@ -168,14 +168,14 @@ public class InitData {
         createBoard("yuroyuro", "교육 플랫폼 강사 모집", "제가 곧 런칭을 준비중인 플랫폼에 네트워크 수업을 해주실 강사분을 모집합니다! 관심 있으신 분들은 쪽지 주세요~",  BoardType.FREE, FreeBoardCategory.PROMOTION, null);
 
         createBoard("benny", "개꿀팁 공유", "다음에 팀장하고 싶은 사람 있냐고 하면 화장실 다녀오셈",  BoardType.FREE, FreeBoardCategory.TIP, null);
-        createBoard("movingGun", "아직도 명령어를 전부 다 치고 있으신가요?", "나만의 리눅스 명령어를 alias 로 등록하여 빠르고 쉽게 배포하세요!",  BoardType.FREE, FreeBoardCategory.TIP, null);
+        createBoard("movinggun", "아직도 명령어를 전부 다 치고 있으신가요?", "나만의 리눅스 명령어를 alias 로 등록하여 빠르고 쉽게 배포하세요!",  BoardType.FREE, FreeBoardCategory.TIP, null);
         createBoard("hyeonah", "공유채팅으로 알고리즘 스터디하기", "알고 계셨나요? 코밋에는 공유 채팅 기능이 있다는 걸! 알고리즘 스터디에서 특히 유용하게 쓸 수 있아요 ㅎㅎ",  BoardType.FREE, FreeBoardCategory.TIP, null);
         createBoard("iammusk", "진짜 개꿀팁 공유", "내가 트윗하면 코인 사셈. ㄹㅇ 개꿀팁임",  BoardType.FREE, FreeBoardCategory.TIP, null);
         createBoard("iamaltman", "챗지피티 요금", "첫 한달은 무료로 사용할 수 있어요! GPT 4 뿐만 아니라 DALLE-3도 쉽게 사용 가능하다고요~ API는 별도 요금이니 참고하세요!",  BoardType.FREE, FreeBoardCategory.TIP, null);
         createBoard("yuroyuro", "화면공유 필터 쓰기", "화면 공유시 얼굴에 다양한 필터를 적용할 수 있어요. 이번 스터디 때 해보니까 재밌더라고요 ㅋㅋ",  BoardType.FREE, FreeBoardCategory.TIP, null);
 
         createBoard("hyeeyon", "JPA 질문", "read-only 트랜잭션 범위를 어디까지 잡아야 할 지 모르겠습니다. 컨트롤러에서 직접 사용하는 퍼블릭 서비스 메소드 기준으로 트랜잭션을 나눠야 할까요, 아니면 protected로 꼭 필요한 부분만 @Transactional을 사용하고 나머지는 read-only로 진행해도 되는걸까요?",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
-        createBoard("movingGun", "쿠버네티스 분석 질문", "쿠버네티스를 많이 사용해보신 분들 있으실까요? 설치 방법이 궁금합니다!",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
+        createBoard("movinggun", "쿠버네티스 분석 질문", "쿠버네티스를 많이 사용해보신 분들 있으실까요? 설치 방법이 궁금합니다!",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
         createBoard("damongsanga", "CSRF 공격 질문", "Referer 검증 외에 CSRF 공격을 막을 수 있는 방법이 무엇이 있을까요?",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
         createBoard("hyeonah", "CORS 에러..", "백과 연동하면서 계속 CORS 에러가 발생합니다.. Access-Control-Allow-Origin 설정을 해야된다고 하는데 어떻게 해야 할까요?",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
         createBoard("iammusk", "DB 선택에 고민이 있습니다.", "이번 프로젝트 때 RDBMS를 MySQL로 할지 PostgreSQL을 사용할지 모르겠어요.. 둘 다 사용해 보신분들 혹시 어떤 DB가 좋을까요?",  BoardType.FREE, FreeBoardCategory.QUESTION, null);
@@ -184,7 +184,7 @@ public class InitData {
 
         // 모집 게시판
         createBoard("damongsanga", "블록체인 프로젝트 참여자", "이더리움 기반의 스마트 컨트랙트 개발 프로젝트입니다. 관심있는 분들의 많은 참여 바랍니다.", BoardType.RECRUIT, null, 7L);
-        createBoard("movingGun", "자바스크립트 고급 기술 탐구", "자바스크립트 프로그래밍의 고급 개념을 탐구할 팀원을 모집합니다.", BoardType.RECRUIT, null, 8L);
+        createBoard("movinggun", "자바스크립트 고급 기술 탐구", "자바스크립트 프로그래밍의 고급 개념을 탐구할 팀원을 모집합니다.", BoardType.RECRUIT, null, 8L);
         createBoard("hyeeyon", "웹 개발 입문", "HTML, CSS, 자바스크립트를 이용한 웹 개발 초보자들 함께해요!.", BoardType.RECRUIT, null, 9L);
         createBoard("yuroyuro", "AWS 클라우드 컴퓨팅", "아마존 웹 서비스를 이용한 클라우드 컴퓨팅 서비스에 대한 토론할 팀원을 모집합니다.", BoardType.RECRUIT, null, 10L);
         createBoard("benny", "사이버 보안 기초", "사이버 보안의 기초와 온라인 존재를 보호하는 방법을 같이 배워봐요.", BoardType.RECRUIT, null, 11L);
@@ -217,7 +217,7 @@ public class InitData {
                 "리액트 네이티브로 모바일 앱 개발해보신 분 계신가요? 경험을 듣고 싶어요."
         );
 
-        createComment(1L, commentsForJava.get(0), "movingGun");
+        createComment(1L, commentsForJava.get(0), "movinggun");
         createComment(1L, commentsForJava.get(0), "hyeeyon");
         createComment(1L, commentsForJava.get(0), "yuroyuro");
         createComment(1L, commentsForJava.get(0), "benny");
@@ -225,7 +225,7 @@ public class InitData {
 
 
         createComment(1L, "제네릭스 부분이 아직 어려운데, 누군가 설명해 줄 수 있나요?", "hyeeyon");
-        createComment(1L, "자바의 가비지 컬렉션에 대해 알게 되어 유익했어요.", "movingGun");
+        createComment(1L, "자바의 가비지 컬렉션에 대해 알게 되어 유익했어요.", "movinggun");
         createComment(1L, "예외 처리 방법에 대한 세션도 추가되면 좋을 것 같아요.", "yuroyuro");
         createComment(1L, "JVM이 실제로 어떻게 작동하는지 궁금했는데, 설명해주셔서 감사합니다!", "damongsanga");
         createComment(1L, "다음 스터디 모임은 언제인가요? 참여하고 싶습니다.", "benny");
@@ -233,7 +233,7 @@ public class InitData {
         createComment(10L, "컴포넌트 라이프사이클에 대해 배울 수 있어서 좋았어요.", "damongsanga");
         createComment(10L, "Hook을 사용한 상태 관리 예제가 정말 도움이 되었습니다.", "benny");
         createComment(10L, "리액트 라우터를 사용한 SPA 구현 부분이 인상적이었어요.", "hyeonah");
-        createComment(10L, "Context API 사용 방법을 자세히 알게 되어서 유익했습니다.", "movingGun");
+        createComment(10L, "Context API 사용 방법을 자세히 알게 되어서 유익했습니다.", "movinggun");
         createComment(10L, "다음 워크샵도 기대되네요. 언제 열리나요?", "yuroyuro");
 
 
