d1d81084650f77c12987aac5eda1a0bbdccedf97
damongsanga <mike0413lata@gmail.com>
2024-02-15 15:57:55 +0900

    Hotfix: 대문자 못씀

--- back/src/main/java/com/a506/comeet/auth/controller/dto/LoginReqeustDto.java
+++ back/src/main/java/com/a506/comeet/auth/controller/dto/LoginReqeustDto.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class LoginReqeustDto {
     @NotNull
+    @Pattern(regexp = "[a-z0-9]*", message = "대문자는 사용할 수 없습니다")
     private String memberId;
     @NotNull
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
